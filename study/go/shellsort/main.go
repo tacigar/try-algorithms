38d8feb8bd5e54b3255ab4027ff4d14500f61d9a
tacigar <ig.lhound@gmail.com>
2023-02-10 20:33:05 +0900

    Update shellsort

--- study/go/shellsort/main.go
+++ study/go/shellsort/main.go
@@ -2,22 +2,26 @@ package main
 
 import "fmt"
 
-func ShellSort(a []int) []int {
+func InsertionSort(a []int, d, s int) []int {
 	n := len(a)
-	hs := []int{8, 4, 2, 1}
-	for h := range hs {
-		for d := 0; d < h; d += 1 {
-			for i := d; i < n; i += h {
-				e := a[i]
-				for j := i - h; j >= 0; j -= h {
-					if a[j] < e {
-						a[j+h] = e
-						break
-					}
-					a[j+h] = a[j]
-					a[j] = e
-				}
+	for i := s; i < n; i += d {
+		e := a[i]
+		for j := i - d; j >= 0; j -= d {
+			if a[j] < e {
+				a[j+d] = e
+				break
 			}
+			a[j+d], a[j] = a[j], a[j+d]
+		}
+	}
+	return a
+}
+
+func ShellSort(a []int) []int {
+	ds := []int{8, 4, 2, 1}
+	for d := range ds {
+		for s := 0; s < d; s++ {
+			InsertionSort(a, d, s)
 		}
 	}
 	return a
