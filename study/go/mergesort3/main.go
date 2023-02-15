f7e74e28980130fedc76cd65bef662bb96990579
tacigar <ig.lhound@gmail.com>
2023-02-15 22:29:57 +0900

    Add mergesort

---
+++ study/go/mergesort3/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func Merge(a, b []int) []int {
+	r := []int{}
+	ai := 0
+	bi := 0
+	for {
+		if ai >= len(a) {
+			r = append(r, b[bi:]...)
+			return r
+		}
+		if bi >= len(b) {
+			r = append(r, a[ai:]...)
+			return r
+		}
+		if a[ai] > b[bi] {
+			r = append(r, b[bi])
+			bi++
+		} else {
+			r = append(r, a[ai])
+			ai++
+		}
+	}
+}
+
+func MergeSort(a []int) []int {
+	if len(a) <= 1 {
+		return a
+	}
+	m := (len(a)) / 2
+	a1 := MergeSort(a[:m])
+	a2 := MergeSort(a[m:])
+	return Merge(a1, a2)
+}
+
+func main() {
+	arr := []int{3, 2, 4, 1, 5, 3, 10, 20, 4, 5, 9, 2, 6, 10, 3}
+	fmt.Println(MergeSort(arr))
+}
