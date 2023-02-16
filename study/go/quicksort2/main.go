58c8bbc0986afc56cb8ccff420747403d234cb1b
tacigar <ig.lhound@gmail.com>
2023-02-17 00:09:18 +0900

    Add quicksort

---
+++ study/go/quicksort2/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func Partition(a []int, l, r int) int {
+	p := a[r]
+	m := l
+	for i := l; i <= r; i++ {
+
+		if a[i] <= p {
+			a[m], a[i] = a[i], a[m]
+			m++
+		}
+		fmt.Println(a, m)
+	}
+	return m - 1
+}
+
+func QuickSortImpl(a []int, l, r int) {
+	if l >= r {
+		return
+	}
+	m := Partition(a, l, r)
+	QuickSortImpl(a, l, m-1)
+	QuickSortImpl(a, m+1, r)
+}
+
+func QuickSort(a []int) {
+	QuickSortImpl(a, 0, len(a)-1)
+}
+
+func main() {
+	// arr := []int{3, 4, 1, 5, 2, 6, 3}
+	arr := []int{3, 2, 4, 1, 5, 3, 10, 20, 4, 5, 9, 2, 6, 10, 3}
+	QuickSort(arr)
+	fmt.Println(arr)
+}
